d428d398c8e7ff7f5922774ff39c8607f1bf2f82
Martin Lichstam <martin.lichstam@gmail.com>
2021-12-13 23:07:39 +0100

    Fix indent

--- 13.js
+++ 13.js
@@ -1,16 +1,26 @@
-const fold = (values = testValues, toFold = ['y=7','x=5' ]) => {
+const fold = (values, toFold) => {
 	if (toFold.length === 0) return values
 	const nextDots = {}
 	const [axis, value] = toFold.shift().split('=')
 	values.forEach((c) => {
 		const [x, y] = c.split(',').map(Number)
 		if (axis === 'x') {
-      nextXy = `${value - Math.abs(x - value)},${y}` 
+			nextXy = `${value - Math.abs(x - value)},${y}` 
 		}
 		else {
-      nextXy = `${x},${value - Math.abs(y - value)}` 
+			nextXy = `${x},${value - Math.abs(y - value)}` 
 		}
 		nextDots[nextXy] = true
 	})
 	return fold(Object.keys(nextDots), toFold)
 }
+
+const paint = (points) => {
+  const msg = Array.from(Array(7), () => Array(40).fill(' '));
+  for (const point of points) {
+    const [x, y] = point.split(',').map(Number);
+    msg[y][x] = '#';
+  }
+  console.log(msg.map(row => row.join('')).join('\n'));
+}
+
